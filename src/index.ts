ed112c78a534338ed0c12f3b0c947c2ab7020bed
Teerut26 <teerutsr@gmail.com>
2022-04-03 15:46:13 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-const nums: number[] = [3, 2, 3]
-const target: number = 6
+console.log("LeetCode");
 
-const check_by_num = (num_n: number, index: number) => {
-    let result: number[] = []
-    nums.map((num, idx) => {
-        if (idx !== index) {
-            if (num_n + num === target) {
-                result.push(idx)
-            }
-        }
-    })
-    return result
-}
-let resutl: number[] = []
-nums.map((num, idx) => {
-    resutl = [...resutl, ...check_by_num(num, idx)]
-})
-console.log(resutl.sort());
 
